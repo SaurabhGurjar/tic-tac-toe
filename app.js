9982a4bb66ad8beedd6b0276f4a2c960083a346c
Saurabh Choudhary <90515478+SaurabhGurjar@users.noreply.github.com>
2023-10-04 00:35:01 +0530

    Rename: module, functions and remove extra if statements

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ const displayController = (() => {
             cell.setAttribute('disabled', true);
         });
     };
-    
+
     const showPlayAgainBtn = () => {
         _playAgainBtn.style.display = 'block';
     };
@@ -65,26 +65,26 @@ const displayController = (() => {
         _playAgainBtn.style.display = 'none';
     }
 
-    
+
     const _showTurnBg = (player) => {
         const element = document.getElementById(player.id);
         element.classList.add('player-turn');
         _showTurnText(player);
     };
-    
+
     const _hideTurnBg = (player) => {
         const element = document.getElementById(player.id);
         element.classList.remove('player-turn');
-        
+
     }
 
     const switchTurn = (player1, player2) => {
-        if(player1.turn) {
-            _showTurnBg(player2);
-            _hideTurnBg(player1);
-        } else {
+        if (player1.turn) {
             _showTurnBg(player1);
             _hideTurnBg(player2);
+        } else {
+            _showTurnBg(player2);
+            _hideTurnBg(player1);
         }
     }
 
@@ -92,7 +92,15 @@ const displayController = (() => {
         _showTurnBg(player);
     }
 
-    const showWinner = (winner, loser) => {
+    const showWinner = (player1, player2) => {
+        let winner, loser;
+        if(player1.win) {
+            winner = player1;
+            loser = player2;
+        } else if(player2.win) {
+            winner = player2;
+            loser = player1;
+        }
         _winnerWrapper.textContent = `${winner.name} wins the game!`;
         _disableCells();
         _hideTurnBg(loser);
@@ -101,7 +109,7 @@ const displayController = (() => {
     }
 
     const showTie = (tie, player1, player2) => {
-        if(tie) {
+        if (tie) {
             _winnerWrapper.textContent = `Tie! no one win the game.`
             showPlayAgainBtn();
             _hideTurnBg(player1);
@@ -130,20 +138,77 @@ const displayController = (() => {
 })();
 
 const Player = (marker, name, id, turn, win) => {
-    return {marker, name, id, turn, win};
+    const positions = [];
+    
+    const setPlayerPos = (pos) => {
+        positions.push(pos);
+    }
+
+    const getPlayerPos = () => {
+       return positions;
+    }
+
+    return { marker, name, id, turn, win, setPlayerPos, getPlayerPos};
 }
 
 
 
 
-const game = (() => {
-    const _boardPos = document.querySelectorAll('.cell');
 
-    const player1 = Player('X', 'Player1', 'player-1', false, false);
-    const player2 = Player('O', 'Player2', 'player-2', true, false);
+
+
+// Game winner check module
+const check = (() => {
+    const _gameBoardArr = gameboard.getPositions();
+    const winningPattern = [[0, 2, 1], [3, 5, 1], [6, 8, 1], [0, 6, 3], [1, 7, 3], [2, 8, 3], [0, 8, 4], [2, 6, 2]]; // THESE ARE THE START, END POSITION AND DIFFERECE BETWEEN POSITION FOR WINNER CHECK LOOP
+
+    const _winnerCheck = (start, end, difference) => {
+        let _count = 0;
+        const mark = _gameBoardArr[start];
+        for (let i = start; i <= end; i += difference) {
+            if (_gameBoardArr[i] !== '') {
+                if (_gameBoardArr[i] === mark) {
+                    _count++;
+                }
+            } else {
+                _count = 0;
+            }
+        }
+        if (_count > 2) {
+            if (mark === game.player1.marker) {
+                game.player1.win = true;
+            } else {
+                game.player2.win = true;
+            }
+        }
+    }
+
+    const winner = () => {
+        const winPatLen = winningPattern.length;
+        for(let i = 0; i < winPatLen; i++) {
+            _winnerCheck(winningPattern[i][0], winningPattern[i][1], winningPattern[i][2]);
+        }
+    };
+    
+    const tie = () => {
+        for (const element of _gameBoardArr) {
+            if (element === '') return false;
+        }
+        return true;
+    };
+    return {
+        winner,
+        tie,
+    }
+})();
+
+const game = (() => {
+
+    const player1 = Player('X', 'Player1', 'player-1', true, false);
+    const player2 = Player('O', 'Player2', 'player-2', false, false);
 
     const _switchTurn = () => {
-        if(player1.turn) {
+        if (player1.turn) {
             player1.turn = false;
             player2.turn = true;
         } else {
@@ -152,105 +217,53 @@ const game = (() => {
         }
     }
 
-    // Definition of get board position function 
-    const _getBoardPos = (callback) => {
-        _boardPos.forEach(element => element.addEventListener('click', (e) => {
-            callback(e.target.dataset.pos);
-        }));
-    };
 
-    // Call get board position function and define callback function
     displayController.switchTurn(player1, player2);
-    _getBoardPos((position) => {
-        const pos = position - 1;
-        _switchTurn();
+
+    const play = (event) => {
+        const pos = event.target.dataset.pos - 1;
         gameboard.setPosition(pos, player1, player2);
+        if (player1.turn) {
+            player1.setPlayerPos(pos);
+        } else if (player2) {
+            player2.setPlayerPos(pos);
+        }
+
         displayController.showMarker(pos);
-        displayController.switchTurn(player1, player2);;
-        displayController.showTie(gameWinner.checkForTie(), player1, player2);
-        gameWinner.checkForWinner();
-    });
-
-    return {
-        player1,
-        player2,
+        _switchTurn();
+        displayController.switchTurn(player1, player2);
+        check.winner();
+        if (game.player1.win || game.player2.win) {
+            displayController.showWinner(game.player1, game.player2); // .showWinner(winner, loser)
+        } else if (check.tie) {
+            displayController.showTie(check.tie(), player1, player2);
+        }
     }
-})();
 
-
-
-// Game winner check module
-const gameWinner = (() => {
-    const _gameBoardArr = gameboard.getPositions();
-
-    const checkForWinner = () => {
-
-        const _check = (start, end, difference) => {
-            let _count = 0;
-            const mark = _gameBoardArr[start];
-            for (let i = start; i <= end; i += difference) {
-                if (_gameBoardArr[i] !== '') {
-                    if (_gameBoardArr[i] === mark) {
-                        _count++;
-                    }
-                } else {
-                    _count = 0;
-                }
-            }
-            if (_count > 2) {
-                if (mark === game.player1.marker) {
-                    game.player1.win = true;
-                    displayController.showWinner(game.player1, game.player2); // .showWinner(winner, loser)
-                } else {
-                    game.player2.win = true;
-                    displayController.showWinner(game.player2, game.player1); 
-                }
-                return;
-            }
-        }
-        const _rowCheck = () => {
-            if(_gameBoardArr[0] !== '' && _gameBoardArr[2] !== '') _check(0, 2, 1);
-            if(_gameBoardArr[3] !== '' && _gameBoardArr[5] !== '') _check(3, 5, 1);
-            if(_gameBoardArr[6] !== '' && _gameBoardArr[8] !== '') _check(6, 8, 1);
-        };
-
-        const _columnCheck = () => {
-            if (_gameBoardArr[0] !== '' && _gameBoardArr[6] !== '') _check(0, 6, 3);
-            if (_gameBoardArr[1] !== '' && _gameBoardArr[7] !== '') _check(1, 7, 3);
-            if (_gameBoardArr[2] !== '' && _gameBoardArr[8] !== '') _check(2, 8, 3);
-        }
-        const _diagonalCheck = () => {
-            if (_gameBoardArr[0] !== '' && _gameBoardArr[8] !== '') _check(0, 8, 4);
-            if (_gameBoardArr[2] !== '' && _gameBoardArr[6] !== '') _check(2, 6, 2);
-        }
-        _columnCheck();
-        _rowCheck();
-        _diagonalCheck();
-    }
-    
-    const checkForTie = () => {
-        for (const element of _gameBoardArr) {
-            if(element === '') return false;
-        }
-        return true;
-    };
-    return {
-        checkForWinner,
-        checkForTie,
-    }
-})();
-
-
-
-const resetDisplay = (() => {
-    const _playAgainBtn = document.getElementById('play-again');
-    _playAgainBtn.addEventListener('click', () => {
+    const reset = () => {
         gameboard.reset();
         displayController.renderCellsData();
-        game.player1.turn = false;
-        game.player2.turn = true;
+        player1.turn = true;
+        player2.turn = false;
+        player1.win = false;
+        player2.win = false
         displayController.hidePlayAgainBtn();
         displayController.showTurn(game.player1);
 
-    });
+    };
+    return {
+        player1,
+        player2,
+        play,
+        reset,
+    }
+})();
+
+
+
+const eventListener = (() => {
+    const _playAgainBtn = document.getElementById('play-again');
+    const _boardPos = document.querySelectorAll('.cell');
+    _boardPos.forEach((cell) => cell.addEventListener('click', game.play));
+    _playAgainBtn.addEventListener('click', game.reset)
 })();
